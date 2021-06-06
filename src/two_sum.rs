2d73ab4081ccf28bd2ee57d2106332a6c9ac34d0
Heron Hu <spectre.hhr@outlook.com>
2021-06-06 10:07:38 +0800

    two sum 解法优化：使用 HashMap 存储已遍历的信息

--- src/two_sum.rs
+++ src/two_sum.rs
@@ -1,17 +1,20 @@
 // https://leetcode-cn.com/problems/two-sum/
 #![allow(dead_code)]
 
-use std::{convert::TryInto, hint};
+use std::{collections::HashMap, hint};
 
 struct Solution;
 
 impl Solution {
     pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        for i in 0..nums.len() {
-            for j in i + 1..nums.len() {
-                if nums[i] + nums[j] == target {
-                    return vec![i.try_into().unwrap(), j.try_into().unwrap()];
-                }
+        let mut cache = HashMap::new();
+
+        for (index, val) in nums.iter().enumerate() {
+            let v = target - *val;
+            if cache.contains_key(&v) {
+                return vec![cache[&v] as i32, index as i32];
+            } else {
+                cache.insert(*val, index);
             }
         }
 
