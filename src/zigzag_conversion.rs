0623536552eec6bf371fe1bd7e9af53589af6980
Heron Hu <spectre.hhr@outlook.com>
2021-06-11 22:00:05 +0800

    ZigZag conversion 优化解法

--- src/zigzag_conversion.rs
+++ src/zigzag_conversion.rs
@@ -1,37 +1,41 @@
 // https://leetcode-cn.com/problems/zigzag-conversion/
 #![allow(dead_code)]
 
-use std::{convert::TryInto, hint};
+use std::convert::TryInto;
 
 struct Solution;
 
 impl Solution {
     pub fn convert(s: String, num_rows: i32) -> String {
-        if num_rows < 2 {
+        if num_rows <= 1 {
             return s;
         }
 
         let s: Vec<_> = s.chars().collect();
         let num_rows: usize = num_rows.try_into().unwrap();
-        let mut rows = Vec::<Vec<char>>::with_capacity(num_rows);
-        for _ in 0..num_rows {
-            rows.push(Vec::with_capacity(s.len()));
-        }
+        let mut result = String::with_capacity(s.len());
         let duration = 2 * (num_rows - 1);
 
-        for i in 0..s.len() {
-            let remainder = i % duration;
-            match remainder {
-                _ if remainder < num_rows => rows[remainder].push(s[i]),
-                _ if remainder >= num_rows => rows[duration - remainder].push(s[i]),
-                _ => unsafe { hint::unreachable_unchecked() },
-            }
-        }
+        for row in 0..num_rows {
+            match row {
+                0 => s.iter().step_by(duration).for_each(|ch| result.push(*ch)),
+                _ if row == num_rows - 1 => {
+                    if s.len() >= num_rows {
+                        s[num_rows - 1..]
+                            .iter()
+                            .step_by(duration)
+                            .for_each(|ch| result.push(*ch))
+                    }
+                }
 
-        let mut result = String::with_capacity(s.len());
-        for row in rows.iter() {
-            for ch in row.iter() {
-                result.push(*ch);
+                _ => (row..s.len()).step_by(duration).for_each(|i| {
+                    result.push(s[i]);
+                    let r = i % duration;
+                    let i = i + duration - 2 * r;
+                    if i < s.len() {
+                        result.push(s[i])
+                    }
+                }),
             }
         }
 
